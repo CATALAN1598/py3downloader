40063dcd25baa06df37625e02aae7416b69af386
CATALAN1598 <acasincallo@gmail.com>
2025-11-25 17:06:26 +0100

    modif gitignore

---
+++ import/py3importer.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+#
+
+#Variable
+readonly SCRIPT=$(realpath -s $0)
+readonly SCRIPT_NAME=${0/%.*/}
+readonly SCRIPT_HOME=${SCRIPT%/*}
+readonly SCRIPT_CONF=${SCRIPT_HOME}/${SCRIPT_NAME}.conf
+readonly SCRIPT_VENV=${SCRIPT_HOME}/.venv/
+readonly SCRIPT_DROP=${SCRIPT_HOME}/drop/
+
+readonly BIN_VENV_PIP=${SCRIPT_VENV%/}/bin/pip
+readonly BIN_VENV_ACT=${SCRIPT_VENV%/}/bin/activate
+readonly BIN_PYTHON3=/usr/bin/python3
+
+#Message
+readonly RED="\e[1;91m"
+readonly BLUE="\e[1;34m"
+readonly GREEN="\e[1;92m"
+readonly YELLOW="\e[0;33m"
+readonly RESET="\e[0m"
+
+readonly INFO="[${BLUE}INFO${RESET}]"
+readonly OK="[${GREEN}OK${RESET}]"
+readonly KO="[${RED}KO${RESET}]"
+readonly DEBUG="[${YELLOW}DEBUG${RESET}]"
+
+source $SCRIPT_CONF
+
+#Function
+
+_say () {
+    local target message
+    target=${1}
+    message=${2}
+    case "$target" in
+        info|i) echo -e "$INFO $message";;
+        ko|k) echo -e "$KO $message";;
+        ok|o) echo -e "$OK $message";;
+        debug|d) echo -e "$DEBUG $message";;
+    esac
+}
+
+_check_apt_dependencies () {
+
+    local dependencie
+
+    for dependencie in "$@"
+    do
+        _say d "Vérification de la présence du package $dependencie prérequis de l'outil"
+        if ! sudo apt list --installed 2> /dev/null | grep "$dependencie\/"
+        then
+            _say d "Le package $dependencie n'est pas installer et est un prérequis de l'outil"
+            _say i "Installation de $dependencie"
+            if ! sudo apt install $dependencie -y  ; then _say k "Installation echoué pour le package $dependencie"; exit 1; fi
+            #if [ "$?" -ne "0" ]; then _say k "Installation echoué pour le package $dependencie"; exit 1; fi
+        else
+            _say o "Le package $dependencie est déjà installé."
+        fi
+    done
+}
+
+_check_virtualenv () {
+
+    if [ ! -d ${SCRIPT_VENV} ]
+    then
+        _say d "Aucun environnement virtuel présent pour l'outil"
+        _say i "Création d'un environnement virtuel pour l'outil"
+        $BIN_PYTHON3 -m venv ${SCRIPT_VENV}
+    else
+        _say i "Un environnement virtuel est configuré dans ${SCRIPT_VENV}"
+    fi
+
+    if [[ -e $BIN_VENV_ACT && -e $BIN_VENV_PIP ]]
+    then
+        _say i "Les outils python3 de l'environnement virtuel sont présent"
+    else
+        _say k "Les outils python3 de l'environnement virtuel ne sont pas présent"
+        _say d "Supprimer l'environnement virtuel (rm -rf ${SCRIPT_VENV}) puis relancer $0"
+        exit 1
+    fi
+}
+
+#Main
